be4aa0b0661ab904878962ef89010e43cd12bee8
Rodrigo Mori <rodrigo.bcunha@claro.com.br>
2025-02-07 22:55:48 -0300

    Ajuste

--- Controller/ReaderController.cs
+++ Controller/ReaderController.cs
@@ -28,28 +28,5 @@ namespace APIInvoiceReader.Controller
     {
       return readerService.Get(request);
     }
-
-    [HttpGet]
-    [Route("HealthCheck")]
-    public void HealthChecksBuilderAddCheckExtensions()
-    {
-      string connectionString = "Server=tcp:invoicereader.database.windows.net;Database=InvoiceReader;User Id=rodrigo.mori;Password=EsCova2309*;";
-
-      // Se estiver usando autenticação do Windows, remova User Id e Password:
-      // string connectionString = "Server=SERVIDOR;Database=MEUBANCO;Integrated Security=True;";
-
-      using (SqlConnection connection = new SqlConnection(connectionString))
-      {
-        try
-        {
-          connection.Open();
-          Console.WriteLine("Conexão bem-sucedida!");
-        }
-        catch (Exception ex)
-        {
-          Console.WriteLine($"Erro na conexão: {ex.Message}");
-        }
-      }
-    }
   }
 }
--- obj/Debug/net8.0/APIInvoiceReader.AssemblyInfo.cs
+++ obj/Debug/net8.0/APIInvoiceReader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIInvoiceReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1b0aa5bac9daded03ab9a33da0b189d7b71fbe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667fe61fbe6f352e5b1e8c609ea7efe2aed748b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIInvoiceReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIInvoiceReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
